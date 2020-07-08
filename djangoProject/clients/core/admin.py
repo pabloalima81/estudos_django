ca5a39fc680c1bac1bbea3c3ce272d2b37b80c22
pabloalima81 <pabloandrades@gmail.com>
2020-07-07 23:09:08 -0300

    trained class today

--- djangoProject/clients/core/admin.py
+++ djangoProject/clients/core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Client
 
-admin.site.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    fields = ['name','last_name', 'age', 'salary']
+    list_display = ['name','last_name', 'age', 'salary']
+    list_filter = ['name','last_name', 'age', 'salary']
+    search_fields = ['last_name', 'age']
+
+admin.site.register(Client, ClientAdmin)
